85041c7757bd3b940d9351616df5232ceec21220
hjyoon <hjyoon@kakao.com>
2024-11-12 05:30:37 +0900

    feat: Enhance ESLint configuration and improve TypeScript setup

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import prettierConfig from "eslint-config-prettier";
 import tseslint from "typescript-eslint";
 
@@ -10,17 +11,22 @@ export default tseslint.config(
   {
     extends: [
       js.configs.recommended,
-      ...tseslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
       prettierConfig,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -28,6 +34,13 @@ export default tseslint.config(
         "warn",
         { allowConstantExport: true },
       ],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+    },
+    settings: {
+      react: {
+        version: "18.3",
+      },
     },
   }
 );
