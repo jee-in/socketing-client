cd03601119b32267bf36ac6882cbb01756c88249
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-13 03:16:43 +0900

    feat: Add login-page (#30)

---
+++ src/components/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import LoginForm from "../organisms/Form/LoginForm";
+import Header from "../templates/header/Header";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Header></Header>
+      <LoginForm></LoginForm>
+    </>
+  );
+};
+
+export default LoginPage;
