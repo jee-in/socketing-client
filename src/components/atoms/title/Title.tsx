aa4a57cd89f418984b2e4484457257cbbd4c03a3
hyeda <skdltm622@gmail.com>
2024-11-12 18:07:18 +0900

    feat: Update title component (#13)

--- src/components/atoms/title/Title.tsx
+++ src/components/atoms/title/Title.tsx
@@ -2,12 +2,14 @@ import styled from "@emotion/styled";
 
 interface TitleProps {
   children: string;
+  textAlign?: "left" | "center" | "right" | "justify";
 }
-const StyledTitle = styled.h1`
+const StyledTitle = styled.h1<{ textAlign?: string }>`
   font-size: 3.5rem;
   font-weight: bold;
   color: black;
   margin: 1rem 0;
+  text-align: ${(props) => props.textAlign || "left"};
 `;
 const Title = ({ children }: TitleProps) => {
   return <StyledTitle>{children}</StyledTitle>;
@@ -15,12 +17,14 @@ const Title = ({ children }: TitleProps) => {
 
 interface SubtitleProps {
   children: string;
+  textAlign?: "left" | "center" | "right" | "justify";
 }
-const StyledSubtitle = styled.h3`
+const StyledSubtitle = styled.h3<{ textAlign?: string }>`
   font-size: 2.5rem;
   font-weight: bold;
   color: black;
   margin: 0.5rem 0;
+  text-align: ${(props) => props.textAlign || "left"};
 `;
 const Subtitle = ({ children }: SubtitleProps) => {
   return <StyledSubtitle>{children}</StyledSubtitle>;
@@ -28,16 +32,18 @@ const Subtitle = ({ children }: SubtitleProps) => {
 
 interface BoldTextProps {
   children: string;
+  textAlign?: "left" | "center" | "right" | "justify";
 }
-const BoldText = ({ children }: BoldTextProps) => {
-  return <b>{children}</b>;
+const BoldText = ({ children, textAlign = "left" }: BoldTextProps) => {
+  return <b style={{ display: "block", textAlign }}>{children}</b>;
 };
 
 interface BodyTextProps {
   children: string;
+  textAlign?: "left" | "center" | "right" | "justify";
 }
-const BodyText = ({ children }: BodyTextProps) => {
-  return <p>{children}</p>;
+const BodyText = ({ children, textAlign = "left" }: BodyTextProps) => {
+  return <p style={{ textAlign }}>{children}</p>;
 };
 
 export { Title, Subtitle, BoldText, BodyText };
