fd877c209fa9752f6c77ac3ff670db4a30e00bb2
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-13 03:02:42 +0900

    feat: Add join-page (#28)

---
+++ src/components/pages/JoinPage.tsx
@@ -0,0 +1,13 @@
+import JoinForm from "../organisms/Form/JoinForm";
+import Header from "../templates/header/Header";
+
+const JoinPage = () => {
+  return (
+    <div>
+      <Header></Header>
+      <JoinForm></JoinForm>
+    </div>
+  );
+};
+
+export default JoinPage;
