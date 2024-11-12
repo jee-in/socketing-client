8852201e855715560a090b3cadfe6b4158f62847
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-13 03:29:51 +0900

    feat: Add reservation-page (#31)

---
+++ src/components/pages/ReservationPage.tsx
@@ -0,0 +1,13 @@
+import SeatContainer from "../organisms/seat-container/SeatContainer";
+import Header from "../templates/header/Header";
+
+const ReservationPage = () => {
+  return (
+    <>
+      <Header></Header>
+      <SeatContainer></SeatContainer>
+    </>
+  );
+};
+
+export default ReservationPage;
