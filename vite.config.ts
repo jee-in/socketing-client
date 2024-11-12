715f67845c8806b93e33f7e3da7b5bd780c261ed
DongHyun Chae <danny001006@naver.com>
2024-11-12 18:36:11 +0900

    feat: Add vite-plugin-svgr (#15)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    svgr({
+      // A minimatch pattern, or array of patterns, which specifies the files in the build the plugin should include.
+      include: "**/*.svg?react",
+    }),
+  ],
 });
