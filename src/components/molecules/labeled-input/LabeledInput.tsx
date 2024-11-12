cf07ebdce07aeb218414f60c6c38ce5e7492d61d
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-12 22:44:24 +0900

    style: Fix import path (#19)

--- src/components/molecules/labeled-input/LabeledInput.tsx
+++ src/components/molecules/labeled-input/LabeledInput.tsx
@@ -1,15 +1,26 @@
-import { BoldText } from "./components/atoms/title/Title";
-import Input, { InputProps } from "./components/atoms/input/Input";
+import { BoldText } from "../../atoms/title/Title";
+import Input, { InputProps } from "../../atoms/input/Input";
 
 export interface LabeledInputProps extends InputProps {
-    label: string;
-  }
+  label: string;
+}
 
-const LabeledInput = ({label, placeholder, value, onChange, width}: LabeledInputProps) => {
+const LabeledInput = ({
+  label,
+  placeholder,
+  value,
+  onChange,
+  width,
+}: LabeledInputProps) => {
   return (
     <div>
       <BoldText>{label}</BoldText>
-      <Input placeholder={placeholder} value={value} onChange={onChange} width={width}></Input>
+      <Input
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        width={width}
+      ></Input>
     </div>
   );
 };
