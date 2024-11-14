5770491d232dc4787e125495426fddec6b3f0802
DongHyun Chae <danny001006@naver.com>
2024-11-14 14:42:11 +0900

    [SOC-102] 메인 페이지 마이페이지 혼동 실수 개선 (#38)
    * feat: Add mock data for event and Refactor event overview temp
    
    * fix: Fix mistake file change

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,29 +1,11 @@
 import Header from "../templates/header/Header";
 import EventOverviewTemplate from "../templates/event-overview/EventOverviewTemplate";
 
-const events = [
-  {
-    title: "콜드 플레이 내한 공연",
-    image:
-      "https://ticketimage.interpark.com/Play/image/large/24/24013437_p.gif",
-  },
-  {
-    title: "두아리파 내한 공연",
-    image:
-      "https://ticketimage.interpark.com/Play/image/large/24/24007623_p.gif",
-  },
-  {
-    title: "크러쉬 공연",
-    image:
-      "https://ticketimage.interpark.com/Play/image/large/24/24015807_p.gif",
-  },
-];
-
 function MainPage() {
   return (
     <>
       <Header />
-      <EventOverviewTemplate title="공연 목록" events={events} />
+      <EventOverviewTemplate />
     </>
   );
 }
