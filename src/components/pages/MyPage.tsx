ee8bac21e55ef94d8ee7f94dae9eb0234557678f
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-15 04:44:07 +0900

    fix: Fix my-page (#46)

--- src/components/pages/MyPage.tsx
+++ src/components/pages/MyPage.tsx
@@ -1,38 +1,11 @@
-import EventOverviewTemplate from "../templates/event-overview/EventOverviewTemplate";
 import Header from "../templates/header/Header";
 
-const Mypage = () => {
-  const events = [
-    {
-      title: "콜드 플레이 내한 공연",
-      image:
-        "https://ticketimage.interpark.com/Play/image/large/24/24013437_p.gif",
-    },
-    {
-      title: "두아리파 내한 공연",
-      image:
-        "https://ticketimage.interpark.com/Play/image/large/24/24007623_p.gif",
-    },
-    {
-      title: "크러쉬 공연",
-      image:
-        "https://ticketimage.interpark.com/Play/image/large/24/24015807_p.gif",
-    },
-  ];
-
+function MyPage() {
   return (
     <>
-      <Header></Header>
-      <EventOverviewTemplate
-        title="예매 목록 (구매자)"
-        events={events}
-      ></EventOverviewTemplate>
-      <EventOverviewTemplate
-        title="주최 목록 (판매자)"
-        events={events}
-      ></EventOverviewTemplate>
+      <Header />
     </>
   );
-};
+}
 
-export default Mypage;
+export default MyPage;
