a0ce42c5608e8382a2fb75381d1a5b7b2dfe5cbb
Yeonu Park <130972950+yeonupark@users.noreply.github.com>
2024-11-15 03:17:24 +0900

    fix: Fix deploy error (#44)

--- src/components/atoms/inputs/Input.tsx
+++ src/components/atoms/inputs/Input.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 import { InputProps } from "../../../types/components/common";
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({
-  placeholder,
-  type = "text",
-  className = "",
-  ...props
-}, ref) => {
-  return (
-    <input
-      ref={ref}
-      type={type}
-      placeholder={placeholder}
-      className={`${className} px-4 py-2 border border-gray-300 rounded-lg text-base w-full focus:outline-none focus:border-rose-400`}
-      {...props}
-    />
-  );
-}
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, type = "text", className = "", ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type={type}
+        placeholder={placeholder}
+        className={`${className} px-4 py-2 border border-gray-300 rounded-lg text-base w-full focus:outline-none focus:border-rose-400`}
+        {...props}
+      />
+    );
+  }
 );
+
+Input.displayName = "Input";
 
 export default Input;
--- src/components/molecules/labeledinput/LabeledInput.tsx
+++ src/components/molecules/labeledinput/LabeledInput.tsx
@@ -1,26 +1,24 @@
-import React from "react"
+import React from "react";
 import Input from "../../atoms/inputs/Input";
 import { LabeledInputProps } from "../../../types/components/common";
 import Font from "../../atoms/fonts/Font";
 
-const LabeledInput = React.forwardRef<HTMLInputElement, LabeledInputProps>(({
-  label,
-  placeholder,
-  width,
-  ...props
-}, ref) => {
-  return (
-    <div>
-      <Font className="text-left">{label}</Font>
-      <Input
-      ref={ref}
-        placeholder={placeholder}
-        width={width}
-        {...props}
-      ></Input>
-    </div>
-  );
-}
+const LabeledInput = React.forwardRef<HTMLInputElement, LabeledInputProps>(
+  ({ label, placeholder, width, ...props }, ref) => {
+    return (
+      <div>
+        <Font className="text-left">{label}</Font>
+        <Input
+          ref={ref}
+          placeholder={placeholder}
+          width={width}
+          {...props}
+        ></Input>
+      </div>
+    );
+  }
 );
+
+LabeledInput.displayName = "LabeledInput";
 
 export default LabeledInput;
