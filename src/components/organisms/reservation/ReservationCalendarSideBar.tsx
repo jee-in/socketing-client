4197c85dddba905126ce970d4b7d430d77ab7453
jeein <159971124+jee-in@users.noreply.github.com>
2024-11-20 03:45:05 +0900

    refactor: Update reservation-page button (#107)

--- src/components/organisms/reservation/ReservationCalendarSideBar.tsx
+++ src/components/organisms/reservation/ReservationCalendarSideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { EventDate } from "../../../types/api/event";
 
 interface DateProps {
@@ -6,19 +6,6 @@ interface DateProps {
 }
 
 const ReservationCalendarSideBar: React.FC<DateProps> = ({ dateData }) => {
-  // 버튼별 상태를 관리하는 배열
-  const [clickedStates, setClickedStates] = useState<boolean[]>(
-    Array(dateData.length).fill(false)
-  );
-
-  const handleClick = (index: number) => {
-    // 클릭된 버튼의 상태만 변경
-    const newClickedStates = clickedStates.map((state, idx) =>
-      idx === index ? true : false
-    );
-    setClickedStates(newClickedStates);
-  };
-
   return (
     <div className="h-full flex justify-center">
       <div className="p-10 space-y-3">
@@ -26,12 +13,7 @@ const ReservationCalendarSideBar: React.FC<DateProps> = ({ dateData }) => {
         {dateData.map((dateData, index) => (
           <button
             key={index}
-            onClick={() => handleClick(index)}
-            className={`w-full p-3 font-bold shadow rounded-lg text-center border  ${
-              clickedStates[index]
-                ? "bg-gray-300"
-                : "bg-white hover:bg-gray-100"
-            }`}
+            className="w-full p-3 font-bold shadow rounded-lg text-center border"
           >
             {new Date(dateData.date)
               .toISOString()
