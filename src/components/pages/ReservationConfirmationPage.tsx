d287239f315b9338a0d9a399aad00b37c1697ce4
jeein <159971124+jee-in@users.noreply.github.com>
2024-12-13 14:15:05 +0900

    fix: Remove survey link in reservation-confirmation page (#221)

--- src/components/pages/ReservationConfirmationPage.tsx
+++ src/components/pages/ReservationConfirmationPage.tsx
@@ -15,21 +15,6 @@ const ReservationConfirmationPage = () => {
         <p className="text-center text-xl md:text-2xl font-bold mb-2">
           {"예매가 완료되었습니다."}
         </p>
-
-        {/* 유저테스트를 위한 */}
-        <div className="flex flex-col items-center justify-center rounded-md h-[100px] bg-gray-100 space-y-2">
-          <label className="text-lg font-bold text-gray-700">
-            ❗️❗️설문조사에 참여해주세요 ❗️❗️🙏
-          </label>
-          <a
-            href="https://docs.google.com/forms/d/e/1FAIpQLSc2kS5zHgkzFog7PYnzRHwRLWjPIGhBEteYToUZ9IZK1PkAFw/viewform"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="px-6 py-2 bg-gradient-to-r from-red-500 to-yellow-500 text-white font-bold rounded-lg shadow-lg hover:from-red-600 hover:to-yellow-600 transition-transform transform hover:scale-105"
-          >
-            설문조사 링크 열기
-          </a>
-        </div>
         <ReservationOverviewTemplate data={paymentData} />
       </div>
     </MainLayout>
